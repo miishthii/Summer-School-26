c53ea18df50ccff9e937054696628b3a52178f98
Mishthi Chopra <24bec042@smvdu.ac.in>
2026-06-27 17:44:06 +0530

    Create led_blinkv1.3.ino
    docs: add file header comments

---
+++ IoT Week1/led_blink/led_blinkv1.3.ino
@@ -0,0 +1,29 @@
+/*
+Author: Mishthi Chopra
+Date: 27 June 2026
+Description: LED blink program with potentiometer speed control and serial blink counter.
+*/
+
+int led = 13;
+int pot = A0;
+int blinkcount = 0;
+
+void setup() {
+  pinMode(led, OUTPUT);
+  Serial.begin(9600);}
+
+void loop() {
+
+  int sensorval = analogRead(pot);
+  int delaytime = map(sensorval, 0, 1023, 100, 1000);
+
+  digitalWrite(led, HIGH);
+  delay(delaytime);
+
+  digitalWrite(led, LOW);
+  delay(delaytime);
+
+  blinkcount++;
+
+  Serial.print("Blink count: ");
+  Serial.println(blinkcount);}
